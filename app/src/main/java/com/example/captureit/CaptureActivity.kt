f6f2238a6d3363724983c5c6706e7b1e9c015fd9
Mantu Kumar Ojha <mk420464@gmail.com>
2023-06-20 23:07:59 +0530

    UI created

--- app/src/main/java/com/example/captureit/CaptureActivity.kt
+++ app/src/main/java/com/example/captureit/CaptureActivity.kt
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.FirebaseApp
 
-class CreateActivity : AppCompatActivity() {
+class CaptureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create)
+        setContentView(R.layout.activity_capture)
 
-        FirebaseApp.initializeApp(this)
+
 
 
     }
